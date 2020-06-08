f7ad410bc106d2f4cb1c160f85412a50d0fd1d6f
sauravyash <yAgasti@gmail.com>
2020-06-09 00:42:51 +1000

    hi

--- index.js
+++ index.js
@@ -7,15 +7,34 @@ const app = express()
 const port = process.env.PORT || 3000
 const url = process.env.URL || "https://invsoc.herokuapp.com"
 
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database('./bot.db')
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./bot.db')
+const sha1 = require('sha1')
+const { v4: uuidv4 } = require('uuid');
 
 const secureData = require('./token.json')
 
 app.get('/', (req, res) => res.send('tryna be a haxor lmao get a life!'))
 
-app.get('/verify/:id', (req, res) => {
-  const id = req.params.id
+app.get('/verify/:code', (req, res) => {
+  const code = req.params.code
+  let id, guild
+  db.each(`SELECT * FROM verification WHERE 'code'='${code}'`, (err, row) => {
+    id = row.id
+    guild = row.guild
+  })
+  
+  if (!id) res.send("Invalid ID!")
+
+  const guildObj = client.guilds?.cache.find(g => g.id === guild)
+  const member = guildObj?.members?.cache.find(m => m.id === id)
+  const verifiedRole = guildObj?.roles?.cache?.find(id => id.name === "Verified") || {
+    id: "719161072640196648",
+    name: "Verified"
+  }
+
+  member.setRoles([verifiedRole])
+  member.send("You have been successfully verified!")
 
 })
 
@@ -31,7 +50,7 @@ client.on('ready', () => {
   //create db
   db.serialize(() => {
     db.each("SELECT name FROM sqlite_master WHERE type='table' AND name='verification';", (err, row) => {
-        if (!row) db.run('CREATE TABLE verification(id code)')
+        if (!row) db.run('CREATE TABLE verification(id code guild)')
         else console.log("Table exists.")
     })
   })
@@ -66,11 +85,11 @@ client.on('message', async msg => {
             }
           })
           
-          let link = `https://invsoc.herokuapp.com/verify/`
-          let code = 1
+          let code = sha1(author.id + uuidv4())
 
-          db.run(`INSERT INTO artists (id, code) VALUES('${author.id}', '${code}')`)
-          
+          db.run(`INSERT INTO artists (id, code, guild) VALUES('${author.id}', '${code}', '${msg.guild.id}')`)
+          let link = `https://invsoc.herokuapp.com/verify/${code}`
+
           let info = await transporter.sendMail({
             from: `UNSW Investment Society`, // sender address
             to: `${msg.content}@ad.unsw.edu.au`, // list of receivers
