f424c6046da10fce6e3a73ac4e54dbb74b3f6f95
sauravyash <yAgasti@gmail.com>
2020-06-11 12:36:20 +1000

    structure

--- server.js
+++ server.js
@@ -24,36 +24,39 @@ const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL || {
   database: secureData.DBdatabase
 })
 
-let queryDB = (query) => new Promise((resolve, reject) => {
-  connection.connect()
-  connection.query(query, function (error, results, fields) {
-    if (error) reject(error)
-    else resolve({results, fields})
-  })
-  connection.end()
-})
-
-app.get('/', (req, res) => res.send('You must be a wannabe hacker hahaha'))
-
-app.get('/verify/:code', async (req, res) => {
-  const code = req.params.code
-  console.log("Verifying:", code)
-
-  const guildObj = client.guilds.cache.find(g => g.id === secureData.guildID)
-  const verifiedRole = guildObj.roles.cache.find(id => id.name === "Verified")
-  
-  await queryDB(`SELECT * FROM verification WHERE code=${connection.escape(code)}`)
-    .then(async re => {
-      const member = guildObj.members.cache.find(m => m.id === re.results[0].user)
-        
-      await member.roles.add(verifiedRole.id)
-      member.send("You have been successfully verified!")
-      res.send("Successfully verified!")
-    }).catch(err => {
-      console.error(err)
-      res.send("Error: Invalid ID!")
+client.on('ready', () => {
+  let queryDB = (query) => new Promise((resolve, reject) => {
+    connection.connect()
+    connection.query(query, function (error, results, fields) {
+      if (error) reject(error)
+      else resolve({results, fields})
     })
+    connection.end()
+  })
+  
+  app.get('/', (req, res) => res.send('You must be a wannabe hacker hahaha'))
+  
+  app.get('/verify/:code', async (req, res) => {
+    const code = req.params.code
+    console.log("Verifying:", code)
+  
+    const guildObj = client.guilds.cache.find(g => g.id === secureData.guildID)
+    const verifiedRole = guildObj.roles.cache.find(id => id.name === "Verified")
+    
+    await queryDB(`SELECT * FROM verification WHERE code=${connection.escape(code)}`)
+      .then(async re => {
+        const member = guildObj.members.cache.find(m => m.id === re.results[0].user)
+          
+        await member.roles.add(verifiedRole.id)
+        member.send("You have been successfully verified!")
+        res.send("Successfully verified!")
+      }).catch(err => {
+        console.error(err)
+        res.send("Error: Invalid ID!")
+      })
+  })
+  
+  app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 client.login(secureData.token)
