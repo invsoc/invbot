cc5510cdbf7585380a7848985e227ab7a5cfafd7
Yash <yagasti@gmail.com>
2020-11-23 13:06:22 +1100

    init

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:14
+
+# set a directory for the app
+WORKDIR /usr/src/app
+
+# copy all the files to the container
+COPY . .
+
+# install dependencies
+RUN npm install
+
+# define the port number the container should expose
+EXPOSE 5000
+
+# run the command
+CMD ["npm", "start"]
+CMD ["npm", "bot"]
--- index.js
+++ index.js
@@ -32,14 +32,75 @@ let queryDB = (query) => new Promise((resolve, reject) => {
   })
 })
 
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+const mysql = require('mysql')
+
+const fs = require('fs')
+const secureData = (fs.existsSync(`./token.json`)) ? require('./token.json') : {
+  "token": process.env.token,
+  "user": process.env.user,
+  "pass": process.env.pass,
+  "host": process.env.host,
+  "port": process.env.smtp_port,
+  "guildID": process.env.guildID
+}
+
+const connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL || {
+  host: secureData.DBhost,
+  user: secureData.DBuser,
+  password: secureData.DBpassword,
+  database: secureData.DBdatabase
+})
+
+let queryDB = (query) => new Promise((resolve, reject) => {
+  connection.query(query, (error, results, fields) => {
+    if (error) reject(error)
+    else resolve({results, fields})
+  })
+})
+
 //
 // DISCORD
 //
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 
-  //create db
-  let res = queryDB('CREATE TABLE IF NOT EXISTS verification(user varchar(100), code varchar(100), guild varchar(100))')
+  // create db
+  try {
+    queryDB('CREATE TABLE IF NOT EXISTS verification(user varchar(100), code varchar(100), guild varchar(100))')
+  } catch (err) {
+    console.error(err)
+  }
+
+  app.get('/', (req, res) => res.send('You must be a wannabe hacker hahaha'))
+  
+  app.get('/verify/:code', async (req, res) => {
+    const code = req.params.code
+    console.log("Verifying:", code)
+  
+    const guildObj = client.guilds.cache.find(g => g.id === secureData.guildID)
+    const verifiedRole = guildObj.roles.cache.find(id => id.name === "Verified")
+    
+    await queryDB(`SELECT * FROM verification WHERE code=${connection.escape(code)}`)
+      .then(async re => {
+        const member = guildObj.members.cache.find(m => m.id === re.results[0].user)
+          
+        await member.roles.add(verifiedRole.id)
+        member.send("You have been successfully verified!")
+        res.send("Successfully verified!")
+      }).catch(err => {
+        console.error(err)
+        res.send("Error: Invalid ID!")
+      })
+  })
+  
+  app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 })
 
 client.on('message', async msg => {
